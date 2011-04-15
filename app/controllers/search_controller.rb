d6467f92d79bc3277ec24934fe0edbe16b060a7e
Fernando Espinosa <fer@ferdev.com>
2011-04-15 16:20:52 +0200

    Uses table from admin user

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
   def index
     @malls = []
+    debugger
     if request.xhr? && params[:lat].present? && params[:lon].present?
-      records = CartoDB::Connection.query "SELECT * FROM centros_compras_euskadi"
+      records = CartoDB::Connection.query "SELECT * FROM centros_compras_euskadi_done"
       @malls = records.rows.reject{|m| m.latitude.nil? || m.longitude.nil? }.map{|m| {:nombre => m.nombre, :cartodb_id => m.cartodb_id, :direccion => m.direccion, :municipio => m.municipio, :latitude => m.latitude, :longitude => m.longitude}}
       render :partial => 'malls' and return
     end
