e02bdd2f674a9af21c2758414eb6f66ed8b894f9
Fernando Espinosa <fer@ferdev.com>
2011-04-15 16:23:50 +0200

    Removes debugger flag

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 class SearchController < ApplicationController
   def index
     @malls = []
-    debugger
     if request.xhr? && params[:lat].present? && params[:lon].present?
       records = CartoDB::Connection.query "SELECT * FROM centros_compras_euskadi_done"
       @malls = records.rows.reject{|m| m.latitude.nil? || m.longitude.nil? }.map{|m| {:nombre => m.nombre, :cartodb_id => m.cartodb_id, :direccion => m.direccion, :municipio => m.municipio, :latitude => m.latitude, :longitude => m.longitude}}
